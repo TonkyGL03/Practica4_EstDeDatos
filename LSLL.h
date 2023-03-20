a67d2f9908191e476a265bcf9c8ab6759692ecc3
Daniel Garlop <123623896+TonkyGL03@users.noreply.github.com>
2023-03-20 10:34:12 -0600

    Create LSLL.h

---
+++ LSLL.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+
+#include "ListException.h"
+
+class List{
+private:
+    int data[500];
+    int last;
+
+    bool isValidPos(const int&);
+    void copyAll(const List&);
+
+public:
+    List();
+    List(const List&);
+
+    bool isEmpty();
+    bool isFull();
+
+    void insertData(const int&, const int&);
+    void deleteData(const int&);
+
+    int getFirstPos();
+    int getLastPos();
+    int getPrevPos(const int&);
+    int getNextPos(const int&);
+
+    int retrieve(const int&);
+
+    std::string toString();
+
+    void print();
+
+    void deleteAll();
+
+    List& operator = (const List&);
+};
+
+#endif // LSLL_H_INCLUDED
